f45f8b4cfd63d640512517030ec3644511cc5410
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-04-22 21:27:53 +0300

    working i18n!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import logo from './logo.svg';
+import de from './pics/de.jpg';
+import en from './pics/en.jpg';
+import fi from './pics/fi.jpg';
+import fr from './pics/fr.jpg';
+import ru from './pics/ru.jpg';
+
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' },
+  ru: { nativeName: 'Русский' },
+  fi: { nativeName: 'Suomi' },
+  fr: { nativeName: 'Français' },
+};
+
+const pics = { de: de, en: en, fi: fi, fr: fr, ru: ru };
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={pics[i18n.resolvedLanguage]}
+          className="App-logo"
+          alt="logo"
+        />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              style={{
+                backgroundColor: 'gold',
+                fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal',
+              }}
+              type="submit"
+              onClick={() => i18n.changeLanguage(lng)}
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.turkuort.fi/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('description.part2')}
         </a>
       </header>
     </div>
